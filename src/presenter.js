e9d44b6a5453fe26e5b27a4148eb1e8952d643d5
Denisse91011 <denissecardenasmayan947@gmail.com>
2025-02-11 13:40:25 -0400

    Tercera version de TDDLabBaseProject-main

--- src/presenter.js
+++ src/presenter.js
@@ -1,17 +1,11 @@
 import sumar from "./sumador.js";
-//import multiplicar from "./multiplicador.js"; // Importamos la función de multiplicación
 import { multiplicar } from "./multiplicador.js";
 
 const first = document.querySelector("#primer-numero");
 const second = document.querySelector("#segundo-numero");
 const form = document.querySelector("#sumar-form");
 const div = document.querySelector("#resultado-div");
-
-// Crear y agregar el botón de multiplicación al formulario
-const multiplyButton = document.createElement("button");
-multiplyButton.textContent = "Multiplicar";
-multiplyButton.type = "button"; // Para evitar que se comporte como un submit
-form.appendChild(multiplyButton);
+const multiplyButton = document.querySelector("#multiplicar-button");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -19,13 +13,12 @@ form.addEventListener("submit", (event) => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>Suma: " + sumar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = `<p>Suma: ${sumar(firstNumber, secondNumber)}</p>`;
 });
 
-// Evento para la multiplicación
 multiplyButton.addEventListener("click", () => {
   const firstNumber = Number.parseInt(first.value);
   const secondNumber = Number.parseInt(second.value);
 
-  div.innerHTML = "<p>Multiplicación: " + multiplicar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = `<p>Multiplicación: ${multiplicar(firstNumber, secondNumber)}</p>`;
 });
